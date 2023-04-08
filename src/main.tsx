7841a405e39a37a091bed73a05539d5142a56e49
Orla Howell <howell.orla@gmail.com>
2023-04-08 01:01:02 +0100

    init commit vite

---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Root } from "./pages/Root";
+import { ErrorPage } from "./pages/Error/Error";
+import { Home } from "./pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
